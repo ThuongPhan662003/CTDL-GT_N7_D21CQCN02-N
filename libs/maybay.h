3e571acf9094094602fb8e4e5f55607737c1e192
thuong <thuong5so55555@gmail.com>
2023-02-23 23:33:48 +0700

    thêm hàm xoá hàm thêm hàm tìm cho maybay.h

--- libs/maybay.h
+++ libs/maybay.h
@@ -3,11 +3,11 @@
 #include <string>
 #define MAXMB 300
 
-class MayBay //@brief MayBay @note abc
+class MayBay 
 {
 private:
     char *SoHieuMB; // @note xây dựng hàm chỉ cho phép nhập 16 kí tự
-    char *LoaiMB;   // @note
+    char *LoaiMB;   // 41 kí tự
     int SoDay;
     int SoDong;
 
@@ -22,6 +22,7 @@ public:
     int getSoDay();
     void setSoDong(int SoDong);
     int getSoDong();
+    void show_MB();
     ~MayBay();
 };
 MayBay::MayBay() {}
@@ -65,6 +66,9 @@ int MayBay::getSoDong()
 {
     return SoDong;
 }
+void MayBay::show_MB(){
+    cout<<getSoHieuMB()<<" "<<getLoaiMB()<<" "<<getSoDay()<<" "<<getSoDong()<<endl;
+}
 
 // typedef struct MAYBAY MB;
 // struct listMB
@@ -77,10 +81,69 @@ int MayBay::getSoDong()
 
 
 
-
-///  ////////////////////////
-struct DSMB
+// Danh sách con trỏ của máy bay
+//======================================================================
+class DSMB
 {
-    int n;
-    MayBay *nodes[MAXMB];
+private:
+    int size=0;
+    MayBay *data[MAXMB];
+public:
+    void setsize(int size);
+    int getsize();
+    void getDSMB();
+    bool Is_Empty(DSMB &DS);
+    bool Is_Full(DSMB &DS);
+    MayBay* New_MB(MayBay &maybay);
+    void Insert_MB(DSMB &DS, MayBay &maybay);
+    void Delete_MB(DSMB &DS, int index);
+    int Find_MB(DSMB &DS,char *SoHieuMB);
 };
+void DSMB::setsize(int size){
+    this->size=size;
+}
+int DSMB::getsize(){
+    return size;
+}
+
+void DSMB::getDSMB(){
+    for(int i=0;i<getsize();i++){
+        data[i]->show_MB();
+    }
+};
+bool DSMB::Is_Empty(DSMB &DS) {
+	return (DS.size == 0 ? true : false);
+}
+
+bool DSMB::Is_Full(DSMB &DS) {
+	return (DS.size >= MAXMB ? true : false);
+}
+
+MayBay* DSMB:: New_MB(MayBay &maybay) {
+	MayBay *p = new MayBay;
+	*p = maybay;
+	return p;
+}
+
+void DSMB::Insert_MB(DSMB &DS, MayBay &maybay) {
+	if (DS.size == MAXMB)
+		return;
+	DS.data[DS.size] = New_MB(maybay);
+	DS.size++;
+}
+void DSMB::Delete_MB(DSMB &DS, int index){
+    delete DS.data[index];
+    for(int i=index;i<DS.size-1;i++){
+        DS.data[index]=DS.data[i+1];
+    }
+    DS.size--;
+}
+int DSMB::Find_MB(DSMB &DS,char *SoHieuMB){
+    for(int i=0;i<DS.size;i++){
+        if(DS.data[i]->getSoHieuMB()==SoHieuMB)
+            return i+1;
+    }
+    return -1;
+}
+
+
