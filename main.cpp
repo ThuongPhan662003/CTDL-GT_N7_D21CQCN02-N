4c2befeda60515cfdffe8d0a7751b1b6eeca95e5
thuong <thuong5so55555@gmail.com>
2023-02-24 13:05:58 +0700

    thuong

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include "libs/libs.h"
 // #include <windows.h>
-
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    // std::cout << "Hello, world!" << std::endl;
 
     HanhKhach hk;
 
@@ -14,6 +13,12 @@ int main()
 
     std::cout << "X: " << IDV;
     VeMayBay *veBay = new VeMayBay("A12", hk);
+    // DSMB dsmb;
+    // // MayBay mb("N21","AIRBUSS",5,20);
+    // // New_MB(mb);
+    // cout<<"Co rong hay khong: ";
+    // cout<<Is_Empty(dsmb)<<endl;
+
 
     return 0;
 }
